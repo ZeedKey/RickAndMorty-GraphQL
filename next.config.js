23d2cb4f0a8d1375e261ea5eaba273051ef37a0f
ZeedKey <the.furriest.man.ever@gmail.com>
2022-08-24 14:56:36 +0400

    :twisted_rightwards_arrows: redirections and domains updated

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['rickandmortyapi.com']
+  },
+  async redirects(){
+    return [
+      {
+        source: '/',
+        destination: '/episodes',
+        permanent: true,
+      },
+      {
+        source: '/episodes/:slug([*^d*])',
+        destination: '/episodes',
+        permanent: true,
+      },
+
+      //  TODO: ...OTHER ROUTES REDIRECTION IMPLEMENTATION
+    ]
+  }
 }
 
 module.exports = nextConfig
