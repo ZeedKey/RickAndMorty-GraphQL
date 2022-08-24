e653431e3397c8d61dffb24f266680dc9a1d9c32
ZeedKey <the.furriest.man.ever@gmail.com>
2022-08-24 15:09:50 +0400

    :sparkles: isMobile hook added

---
+++ src/hooks/useIsMobile.tsx
@@ -0,0 +1,16 @@
+import { useLayoutEffect, useState } from "react";
+import debounce from "lodash/debounce";
+
+export const useIsMobile = (): boolean => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useLayoutEffect(() => {
+    const updateSize = (): void => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    window.addEventListener("resize", debounce(updateSize, 250));
+    return (): void => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  return isMobile;
+};
