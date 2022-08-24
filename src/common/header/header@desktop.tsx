6c1a90d035c15d91ef66e10c99b659fa744d7f9b
ZeedKey <the.furriest.man.ever@gmail.com>
2022-08-24 15:02:00 +0400

    :sparkles: header added

---
+++ src/common/header/header@desktop.tsx
@@ -0,0 +1,25 @@
+import { Link } from '@common'
+import { ROUTES } from '@routes'
+import { LogoIcon } from '@icons'
+import styles from './header.module.scss'
+
+const RenderMenuLink = (route: Routes.RouteType) => (
+    <Link href={route.path} key={route.path}>
+        <a className={styles.menu__link}>
+            {route.name}
+        </a>
+    </Link>
+)
+
+export const DesktopHeader: React.FC = () => {
+    return (
+        <header className={styles.header}>
+            <nav className={styles.navbar}>
+                <figure>
+                    <LogoIcon />
+                </figure>
+                <div className={styles.menu}>{ROUTES.map(RenderMenuLink)}</div>
+            </nav>
+        </header>
+    )
+}
